d88e9fda60cd154a91d2b8850d6a36d95fe95775
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-06-25 09:38:08 -0300

    função dos filtros no estado feita

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -3,16 +3,22 @@ import PropTypes from 'prop-types';
 import getApi from '../API/index';
 import contextPlanets from './contextPlanets';
 
-const INITIAL_FILTER_NAME = {
+const INITIAL_FILTER = {
   filterByName: {
     name: '',
   },
   filterByNumericValues: [],
 };
+
+const INITIAL_FILTER_NUMERIC = {
+  column: '',
+  comparison: '',
+  value: '',
+};
 function PlanetsProvider({ children }) {
   const [data, setData] = useState([]);
-  const [filters, setFilter] = useState(INITIAL_FILTER_NAME);
-
+  const [filters, setFilter] = useState(INITIAL_FILTER);
+  const [filterNumeric, setFilterNumeric] = useState(INITIAL_FILTER_NUMERIC);
   const getPlanets = () => {
     getApi().then((results) => setData(results));
   };
@@ -24,14 +30,18 @@ function PlanetsProvider({ children }) {
       filterByName:
       { name: value } });
   };
-  const dropdownFilter = ({ tager: { value } }) => {
-
+  const numericFilter = ({ tager: { name, value } }) => {
+    setFilterNumeric({
+      ...filterNumeric,
+      [name]: value,
+    });
   };
 
   const context = {
     data,
     filters,
     searchPlanetsName,
+    numericFilter,
   };
   return (
     <contextPlanets.Provider value={ context }>
