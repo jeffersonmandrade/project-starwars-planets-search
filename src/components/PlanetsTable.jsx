1716c9b551d9e21c283b059863c3fd658165d5a7
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-06-25 09:10:18 -0300

    terminado o menu do select

--- src/components/PlanetsTable.jsx
+++ src/components/PlanetsTable.jsx
@@ -2,44 +2,37 @@ import React, { useContext } from 'react';
 import contextPlanets from '../context/contextPlanets';
 
 function PlanetsTable() {
-  const { data } = useContext(contextPlanets);
+  const { data, filters } = useContext(contextPlanets);
+  const headers = ['Name', 'Rotation Period', 'Orbital Period', 'Diameter', 'Climate',
+    'Gravity', 'Terrain', 'Surface Water', 'Population', 'Films', 'Created', 'Edit',
+    'Url'];
   return (
     <table>
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>Url</th>
+          {headers.map((header) => (
+            <th key={ header }>{header}</th>))}
         </tr>
       </thead>
       <tbody>
-        {data.map((planet) => (
-          <tr key={ planet.name }>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>{planet.films}</td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
-          </tr>
-        ))}
+        {data.filter((element) => element.name.includes(filters.filterByName.name))
+          .map((planet) => (
+            <tr key={ planet.name }>
+              <td>{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>{planet.films}</td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))}
       </tbody>
     </table>
 
--- src/components/SearchPlanets.jsx
+++ src/components/SearchPlanets.jsx
@@ -3,11 +3,33 @@ import contextPlanets from '../context/contextPlanets';
 
 function SearchPlanets() {
   const { searchPlanetsName } = useContext(contextPlanets);
-  console.log(searchPlanetsName);
+  const options = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+
   return (
-    <label htmlFor="idSearchBar">
-      <input id="idSearchBar" type="text" onChange={ searchPlanetsName } />
-    </label>
+    <>
+      <label htmlFor="idSearchBar">
+        <input
+          id="idSearchBar"
+          type="text"
+          onChange={ searchPlanetsName }
+          data-testid="name-filter"
+        />
+      </label>
+      <label htmlFor="idSelect">
+        <select
+          id="idSelect"
+          data-testid="column-filter"
+        >
+          {options.map((element) => (
+            <option
+              key={ element }
+            >
+              {element}
+
+            </option>))}
+        </select>
+      </label>
+    </>
   );
 }
 
